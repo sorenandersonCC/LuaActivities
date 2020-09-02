eea277d0190801afe7031b885b0b5106d7af086e
sorenandersonCC <70664742+sorenandersonCC@users.noreply.github.com>
2020-09-02 13:11:30 -0700

    Add files via upload

---
+++ dance.lua
@@ -0,0 +1,10 @@
+while true do
+   turtle.up()
+   turtle.down()
+   turtle.turnLeft()
+   turtle.turnLeft()
+   turtle.forward()
+   turtle.back()
+   turtle.turnLeft()
+   turtle.turnLeft()
+end
---
+++ movement.lua
@@ -0,0 +1,20 @@
+-- Basic Turtle Movement
+turtle.forward()
+turtle.back()
+turtle.turnLeft()
+turtle.turnRight()
+turtle.up()
+turtle.down()
+
+-- Basic Turtle Actions
+turtle.place()
+turtle.dig()
+turtle.attack()
+
+-- Advanced Turtle Actions
+turtle.select(n)
+turtle.placeUp()
+turtle.placeDown()
+turtle.digUp()
+turtle.digDown()
+
